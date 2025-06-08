0780a4bdb593b79b5308ce94a94df06a222d3b81
reinout91 <reinout91@gmail.com>
2025-06-08 17:49:52 +0200

    cleanup

--- lampapy/klein_bottle.py
+++ lampapy/klein_bottle.py
@@ -4,12 +4,12 @@ from build123d import (
     BuildLine,
     BuildPart,
     BuildSketch,
-    Circle,
+    Face,
+    GeomType,
     Line,
     Mode,
     Side,
     __version__,
-    export_stl,
     make_face,
     offset,
     revolve,
@@ -21,6 +21,7 @@ set_port(3939)
 
 print(__version__)
 segment_count = 6
+
 
 with BuildPart() as klein_bottle_outside:
     with BuildSketch():
@@ -37,6 +38,10 @@ with BuildPart() as klein_bottle_outside:
         make_face()
     revolve(axis=Axis.Y, clean=False)
 
+    ft = klein_bottle_outside.part.faces().filter_by(GeomType.PLANE).sort_by(Axis.Y)
+    outer_wires = [Face(outer_wire=face.outer_wire()) for face in ft]
+    inner_wires = [Face(outer_wire=face.inner_wires()[-1]) for face in ft]
+
     with BuildSketch():
         with BuildLine() as handle_center_line:
             h = Bezier(
@@ -47,43 +52,12 @@ with BuildPart() as klein_bottle_outside:
                 (0, 240),
             )
 
-    for i in range(segment_count + 1):
-        with BuildSketch(handle_center_line.line ^ (i / segment_count)) as section:
-            if i == 0:
-                Circle(20)
-            if i == segment_count:
-                Circle(30)
-            else:
-                Circle(20)
-    sections = klein_bottle_outside.pending_faces
+    sweep(sections=outer_wires, path=handle_center_line, multisection=True)
+    sweep(
+        sections=inner_wires,
+        path=handle_center_line,
+        multisection=True,
+        mode=Mode.SUBTRACT,
+    )
 
-    sweep(sections=sections, path=handle_center_line, multisection=True)
-
-
-with BuildPart(mode=Mode.SUBTRACT) as klein_bottle_inside:
-    with BuildSketch():
-        with BuildLine() as handle_center_line:
-            h = Bezier(
-                (0, -140),
-                (0, 0),
-                (510, 440),
-                (0, 440),
-                (0, 240),
-            )
-
-    for i in range(segment_count + 1):
-        with BuildSketch(handle_center_line.line ^ (i / segment_count)) as section:
-            if i == 0:
-                Circle(5)
-            if i == segment_count:
-                Circle(20)
-            else:
-                Circle(10)
-    sections = klein_bottle_inside.pending_faces
-
-    sweep(sections=sections, path=handle_center_line, multisection=True)
-
-klein_bottle = klein_bottle_outside.part - klein_bottle_inside.part
-
-export_stl(klein_bottle, "klein_bottle.stl")
 show_all(reset_camera=Camera.KEEP)
